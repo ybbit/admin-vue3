62d5fced18b78f6b706028d307b0e6a75bdd6d2b
ly <startqbb@163.com>
2023-06-12 00:37:40 +0800

    feat:首次提交

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Descripttion: 我也有梦想呀
+ * @Version: 1.0.0
+ * @Author: L&Y (个人博客:https://www.cnblogs.com/ybbit)
+ * @Date: 2023-06-12 00:14:15
+ * @LastEditors: L&Y
+ * @LastEditTime: 2023-06-12 00:29:38
+-->
+<template>
+    <div id='container'>
+        <h2>
+            我是 App
+        </h2>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+
+</script>
+<style scoped></style>
